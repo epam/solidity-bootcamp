f3de8af1e769507a23d6f6dfd89c11d52cc184f9
Luis Mastrangelo <luismastrangelo@gmail.com>
2022-04-08 01:07:53 +0200

    Add support to work with different forks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,9 +3,41 @@ import { task } from 'hardhat/config';
 import '@nomiclabs/hardhat-waffle';
 import 'hardhat-gas-reporter';
 
+/**
+ * 
+ */
+const chains = {
+  fuji: {
+    url: 'https://api.avax-test.network/ext/bc/C/rpc',
+    chainId: 43113,
+    forking: {
+      blockNumber: 7461390
+    },
+    contracts: {
+      usdcAddr: '0x5425890298aed601595a70AB815c96711a31Bc65',
+    }
+  }
+};
+
+/**
+ * Defines the chain config to use based on the value of the `FORK`
+ * environment variable.
+ * It can be `undefined`.
+ */
+const chain = (() => {
+  const FORK = process.env.FORK;
+  if (!FORK) {
+    return undefined;
+  } else if (FORK in chains) {
+    return chains[FORK as keyof typeof chains];
+  }
+  console.error(`Forking from chain '${FORK}' not supported, set \`FORK\` to one of the following: ${Object.keys(chains)}\n`);
+  process.exit(1);
+})();
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+task("accounts", "Prints the list of accounts", async (_taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
@@ -22,20 +54,19 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 export default {
   solidity: "0.8.4",
   networks: {
-    hardhat: {
-      // forking: {
-      //   url: 'https://api.avax-test.network/ext/bc/C/rpc',
-      //   blockNumber: 7461390,
-      // },
-      // chainId: 43113,
-    },
+    hardhat: chain ? {
+      forking: {
+        url: chain.url,
+        blockNumber: chain.forking.blockNumber,
+      },
+      chainId: chain.chainId,
+    } : {},
     fuji: {
-      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      url: chains.fuji.url,
       gasPrice: 225000000000,
-      chainId: 43113,
+      chainId: chains.fuji.chainId,
       accounts: process.env.PRIVATE_KEY !== undefined ? ['' + process.env.PRIVATE_KEY] : [],
     },
-
   },
   gasReporter: {
     enabled: false,
