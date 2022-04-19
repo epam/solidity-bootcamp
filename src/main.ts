eac3392797df0be97b16276ec3cf9328ebe85a8f
Luis Mastrangelo <luismastrangelo@gmail.com>
2022-04-19 15:12:33 +0200

    Add type def for `window.ethereum`

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import ldtoken from '../artifacts/contracts/LDToken.sol/LDToken.json'
 
 function deploy() {
     console.info('metamask provider', window.ethereum);
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const provider = new ethers.providers.Web3Provider(window.ethereum as any);
 
     const token = new Contract('0x5fbdb2315678afecb367f032d93f642f64180aa3', ldtoken.abi, provider.getSigner());
     return token;
